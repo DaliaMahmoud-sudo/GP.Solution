f32c524151558f918e07a49cbe46587495c51805
MohamedLawendyy <mohamed.h.lawendy@gmail.com>
2025-05-15 02:32:10 +0300

    Fix deployment issues

--- GP.APIs/Program.cs
+++ GP.APIs/Program.cs
@@ -110,11 +110,9 @@ namespace GP.APIs
             #endregion
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
 
             //app.UseHttpsRedirection();
 
